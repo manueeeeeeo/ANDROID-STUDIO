2c0f23a699b2ed0ee75556d643aab4c6766db513
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-03 19:24:54 +0200

    Update Condicionales.kt

--- CURSO/Básico/Condicionales.kt
+++ CURSO/Básico/Condicionales.kt
@@ -1,1 +1,22 @@
+package com.example.kotlinparaprincipiantes
 
+public fun main(){
+    condicionales1()
+}
+
+private fun condicionales1(){
+    var numero = 9
+
+    if (numero>10) {
+        println("$numero es mayor de 10")
+    }else if(numero<10){
+        println("$numero es menor de 10")
+    }
+  // Operadores más complejos
+    /*
+        && agregar condicion extra
+        || agregar una u otra
+        != distinto
+        == igual
+    */
+}
