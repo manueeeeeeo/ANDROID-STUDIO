87afffba37200063bff7dbef2d5eb9f02cde03ab
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-03 19:23:03 +0200

    Update variables.kt

--- CURSO/Básico/variables.kt
+++ CURSO/Básico/variables.kt
@@ -1,1 +1,21 @@
+package com.example.kotlinparaprincipiantes
 
+public fun main(){
+    variablesYConastantes()
+}
+
+private fun variablesYConastantes(){
+    // Variables
+    var miPrimeraVariable = "Hola Manuel"
+
+    println(miPrimeraVariable)
+
+    //Cambiamos de valor a la avriable
+    miPrimeraVariable = "Adios"
+
+    println(miPrimeraVariable)
+
+    // CONSTANTES
+    val primeraConstante = "Te ha gustado?"
+
+}
