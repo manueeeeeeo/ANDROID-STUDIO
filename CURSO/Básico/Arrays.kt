608964f650cee3833cf44afad130451c51435b5a
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-04 14:37:33 +0200

    Update Arrays.kt

--- CURSO/Básico/Arrays.kt
+++ CURSO/Básico/Arrays.kt
@@ -1,1 +1,21 @@
+package com.example.kotlinparaprincipiantes
 
+public fun main(){
+    arrays()
+}
+
+private fun arrays(){
+    val name = "Manuel"
+    val surname = "Sanchez"
+    val company = "HP"
+    val age = "20"
+
+    val myArray = arrayListOf<String>()
+
+    myArray.add(name)
+    myArray.add(surname)
+    myArray.add(company)
+    myArray.add(age)
+
+    println(myArray)
+}
