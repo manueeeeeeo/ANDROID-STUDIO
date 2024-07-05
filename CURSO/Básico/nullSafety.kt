a053cf4b4790ddc029717ab4ba85fe32fc5c9141
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-05 15:41:12 +0200

    Update nullSafety.kt

--- CURSO/Básico/nullSafety.kt
+++ CURSO/Básico/nullSafety.kt
@@ -1,1 +1,28 @@
+package com.example.kotlinparaprincipiantes
 
+import kotlin.random.Random
+
+public fun main(){
+    nullSafety()
+}
+
+private fun nullSafety(){
+    var myString = "Manuel"
+    // myString = null Esto daría un error de compilación
+    println(myString)
+
+    // Variable null safety
+    var mySafetyString: String? = "Manuel null safety"
+    mySafetyString = null
+    println(mySafetyString)
+
+    mySafetyString = "Suscribete"
+    println(mySafetyString)
+
+    if(mySafetyString != null){
+        println(mySafetyString!!) // !! redundancia por si es nulo
+    }
+
+    // Safe call
+    println(mySafetyString.length)
+}
