d1b28002709db48367b4147906c6d690e626d45b
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:58:13 +0200

    Update Main.kt

--- CURSO/Básico/Practica Clases/Main.kt
+++ CURSO/Básico/Practica Clases/Main.kt
@@ -1,1 +1,12 @@
+package com.example.kotlinparaprincipiantes
 
+public fun main(){
+    // Declaro el objeto que simula ser un perro
+    val perro1 = Perro("Sasha", 1, arrayOf(Perro.Vacuna.Rabia, Perro.Vacuna.Parasitos), "Yorkshire Toy", "España")
+    // Vemos la información del perro
+    perro1.imprimir_Info()
+    // Vemos todas las vacunas
+    perro1.ver_Vacunas()
+    // Damos de comer al perro
+    perro1.darDeComer()
+}
