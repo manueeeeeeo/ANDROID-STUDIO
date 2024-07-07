2ecfc032f0faf447895ddc5b4176b9b23ede913f
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:35:58 +0200

    Update Main.kt

--- CURSO/Básico/Clase Tutorial/Main.kt
+++ CURSO/Básico/Clase Tutorial/Main.kt
@@ -1,1 +1,14 @@
+package com.example.kotlinparaprincipiantes
 
+import com.example.kotlinparaprincipiantes.ui.theme.MyClass
+
+public fun main(){
+    // Clases
+    clases()
+}
+
+public fun clases(){
+    val manu = MyClass("Alvaro", 18,  arrayOf(MyClass.Languague.Java, MyClass.Languague.Kotlin))
+    println(manu.name)
+    manu.code()
+}
