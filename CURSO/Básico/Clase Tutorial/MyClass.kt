0964671448f6e9c34329b2148845250192ae7b1e
Manuel <146026153+manueeeeeeo@users.noreply.github.com>
2024-07-07 15:35:33 +0200

    Update MyClass.kt

--- CURSO/Básico/Clase Tutorial/MyClass.kt
+++ CURSO/Básico/Clase Tutorial/MyClass.kt
@@ -1,1 +1,18 @@
+package com.example.kotlinparaprincipiantes.ui.theme
 
+class MyClass(val name: String, val age: Int, val languages: Array<Languague>, val friends: Array<MyClass>? = null) {
+  // Creamos esta lista de lenguajes para que solo puedan introducir esos al crear la clase  
+  enum class Languague{
+        Kotlin,
+        Java,
+        Python,
+        JavaScript
+    }
+
+  // Método para imprimir todos los lenguajes en los que programa
+    fun code(){
+        for(language in languages){
+            println("Programo en $language")
+        }
+    }
+}
